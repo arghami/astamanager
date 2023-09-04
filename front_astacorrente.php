96020ac1b32a200db1042599a77c2e18d6a8ee98
frbiag <biagiotti.f@gmail.com>
2023-09-04 14:12:16 +0200

    Update front_astacorrente.php
    verificherei se le chiamate all'archivio.class.php restituiscono un valore null oppure no (se restituiscono un valore nullo vuol dire che ancora non è stato chiamato all'asta alcun giocatore oppure che non ne è stato acquistato ancora nessuno.

--- front_astacorrente.php
+++ front_astacorrente.php
@@ -15,31 +15,45 @@ $archivio = new Archivio();
 //in questo caso devo leggere il giocatore attualmente in asta
 $astaAttuale = $archivio->getAsta();
 
-//verifico che la foto sia nella dira
-$directory = 'theme/photos/';
-$wtlf = strval($astaAttuale['cod_giocatore']);
-$ext = '.jpg';
-     if( ! file_exists( $directory . $wtlf . $ext) )
-     {
-	 $photo = 'misterx.jpg';
-     } else {
-	 $photo = $astaAttuale['cod_giocatore'].$ext;
-	 }
-	 
-	 
-
-echo  "<table class=\"ora\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" summary=\"\">
- <tr>
-  <td colspan=\"2\" class=\"intest_Now\">ATTUALMENTE ALL'ASTA</td>
-  <td rowspan=\"3\" class=\"fotoG\"><img class=\"foto\" src=\"theme/photos/{$photo}\" /></td>
- </tr>
- <tr>
-  <td class=\"nome_Now\">".tronca($astaAttuale['nome'],20)."</td>
-  <td class=\"ruoloO\">{$astaAttuale['ruolo']}</td>
- </tr>
- <tr>
-  <td colspan=\"2\" class=\"squa_Now\">{$astaAttuale['squadra']}</td>
- </tr>
-</table>";
-
+// controllo se c'è un giocatore all'asta
+if ($astaAttuale === null) {
+    // Il valore restituito è nullo, non ci sono giocatori all'asta
+	echo  "<table class=\"ora\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" summary=\"\">
+	 <tr>
+	  <td colspan=\"2\" class=\"intest_Now\">ATTUALMENTE ALL'ASTA</td>
+	  <td rowspan=\"3\" class=\"fotoG\"><img class=\"foto\" src=\"theme/photos/misterx.jpg\" /></td>
+	 </tr>
+	 <tr>
+	  <td class=\"nome_Now\">Nessun giocatore all'asta</td>
+	  <td class=\"ruoloO\">&nbsp;</td>
+	 </tr>
+	 <tr>
+	  <td colspan=\"2\" class=\"squa_Now\">&nbsp;</td>
+	 </tr>
+	</table>";
+} else {
+	//verifico che la foto sia nella dir
+	$directory = 'theme/photos/';
+	$wtlf = strval($astaAttuale['cod_giocatore']);
+	$ext = '.jpg';
+		 if( ! file_exists( $directory . $wtlf . $ext) )
+		 {
+		 $photo = 'misterx.jpg';
+		 } else {
+		 $photo = $astaAttuale['cod_giocatore'].$ext;
+		 }
+	echo  "<table class=\"ora\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" summary=\"\">
+	 <tr>
+	  <td colspan=\"2\" class=\"intest_Now\">ATTUALMENTE ALL'ASTA</td>
+	  <td rowspan=\"3\" class=\"fotoG\"><img class=\"foto\" src=\"theme/photos/{$photo}\" /></td>
+	 </tr>
+	 <tr>
+	  <td class=\"nome_Now\">".tronca($astaAttuale['nome'],20)."</td>
+	  <td class=\"ruoloO\">{$astaAttuale['ruolo']}</td>
+	 </tr>
+	 <tr>
+	  <td colspan=\"2\" class=\"squa_Now\">{$astaAttuale['squadra']}</td>
+	 </tr>
+	</table>";
+}
 ?>
